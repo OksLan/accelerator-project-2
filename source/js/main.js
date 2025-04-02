d4d105c30377e023513ecf7ef5802c65f0d1e784
Oksana Lanovenko <158132001+OksLan@users.noreply.github.com>
2025-04-02 14:23:51 +0400

    02/04 (1)

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,26 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+
+/* развернуть/свернуть main-nav */
+// document.addEventListener("DOMContentLoaded", () => {
+//     const toggleButton = document.querySelector(".menu__toggle");
+//     const navList = document.querySelector(".main-nav");
+//     const icon = toggleButton.querySelector("svg use");
+
+//     if (!toggleButton || !navList || !icon) {
+//         console.error("Ошибка: один из элементов не найден!");
+//         return;
+//     }
+
+//     toggleButton.addEventListener("click", () => {
+//         const isOpen = navList.classList.toggle("main-nav--opened");
+
+//         toggleButton.classList.toggle("menu__toggle--open", isOpen);
+//         toggleButton.classList.toggle("menu__toggle--close", !isOpen);
+
+//         icon.setAttribute("href", isOpen ? "img/spritemap#cross" : "img/spritemap#menu");
+//     });
+// });
+
