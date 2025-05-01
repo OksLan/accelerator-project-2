a1d47d48e4af78e5132809a4fe2a738f177411a0
Oksana Lanovenko <okslan@gmail.com>
2025-05-01 21:32:10 +0400

    01/05 (3)

--- source/js/cards.js
+++ source/js/cards.js
@@ -43,53 +43,6 @@ const cards = [
 ];
 export { cards };
 
-// const catalog = document.querySelector(".tours__list");
-// const cardTemplate = document.querySelector("#card").content; //template
-
-// let addCard = (card) => {
-//   const cardElement = cardTemplate
-//     .querySelector(".tours__list-item")
-//     .cloneNode(true);
-
-//   // Изображения
-//   cardElement.querySelector(".card__image-webp").srcset = `img/cards/${card.foto}.webp, img/cards/${card.foto_2x}.webp`;
-//   cardElement.querySelector(".card__image-png").srcset = `img/cards/${card.foto}.png, img/cards/${card.foto_2x}.png`;
-//   cardElement.querySelector(".card__image").src = `img/cards/${card.foto}.png`;
-//   cardElement.querySelector(".card__image").srcset = `img/cards/${card.foto_2x}.png`;
-
-//   // Заголовок с HTML-разметкой
-//   cardElement.querySelector(".card__title").innerHTML = card.title;
-
-//   // Локация
-//   cardElement.querySelector(".card__location-text").textContent = card.location;
-
-//   // Продолжительность
-//   cardElement.querySelector(".card__days-text").textContent = card.days;
-//   cardElement.querySelector(".card__nights-text").textContent = card.nights;
-
-//   // Сложность (звезды)
-//   const starsIcon = cardElement.querySelector(".card__stars-icon");
-//   starsIcon.innerHTML = ""; // очищаем
-//   const starsUse = document.createElementNS("http://www.w3.org/2000/svg", "use");
-//   starsUse.setAttribute("href", `/__spritemap#sprite-stars-${card.stars}`);
-//   starsIcon.appendChild(starsUse);
-
-//   // Даты
-//   cardElement.querySelector(".card__dates-dates").textContent = card.dates;
-
-//   // Группа
-//   cardElement.querySelector(".card__group-number").textContent = card.group;
-
-//   catalog.append(cardElement);
-// };
-
-
-// export function showCards(cards) {
-//   for (const card of cards) {
-//     addCard(card);
-//   }
-// }
-
 const catalog = document.querySelector(".tours__list");
 const cardTemplate = document.querySelector("#card").content;
 
