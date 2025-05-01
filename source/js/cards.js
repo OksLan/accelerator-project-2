4b572cc98cd6047a605ce7fd8cdd97750949e728
Oksana Lanovenko <okslan@gmail.com>
2025-05-01 21:31:51 +0400

    01/05 (2)

--- source/js/cards.js
+++ source/js/cards.js
@@ -1,7 +1,6 @@
 const cards = [
   {
-    foto: "elbrus@1x",
-    foto_2x: "elbrus@2x",
+    foto: "elbrus",
     title: "Путешествие<br>вокруг Эльбруса",
     location: "Кавказ",
     days: "4 дня",
@@ -12,8 +11,7 @@ const cards = [
   },
 
   {
-    foto: "belukha@1x",
-    foto_2x: "belukha@2x",
+    foto: "belukha",
     title: "Путь к подножию<br>Белухи",
     location: "Алтай",
     days: "14 дней",
@@ -23,8 +21,7 @@ const cards = [
     group: "до 25 чел.",
   },
   {
-    foto: "fisht@1x",
-    foto_2x: "fisht@2x",
+    foto: "fisht",
     title: "Через Фишт<br>к Чёрному морю <br>и обратно",
     location: "Адыгея",
     days: "7 дней",
@@ -34,8 +31,7 @@ const cards = [
     group: "до 22 чел.",
   },
   {
-    foto: "manaraga@1x",
-    foto_2x: "manaraga@2x",
+    foto: "manaraga",
     title: "Восхождение<br>Манарага",
     location: "Урал",
     days: "9 дней",
@@ -47,46 +43,105 @@ const cards = [
 ];
 export { cards };
 
+// const catalog = document.querySelector(".tours__list");
+// const cardTemplate = document.querySelector("#card").content; //template
+
+// let addCard = (card) => {
+//   const cardElement = cardTemplate
+//     .querySelector(".tours__list-item")
+//     .cloneNode(true);
+
+//   // Изображения
+//   cardElement.querySelector(".card__image-webp").srcset = `img/cards/${card.foto}.webp, img/cards/${card.foto_2x}.webp`;
+//   cardElement.querySelector(".card__image-png").srcset = `img/cards/${card.foto}.png, img/cards/${card.foto_2x}.png`;
+//   cardElement.querySelector(".card__image").src = `img/cards/${card.foto}.png`;
+//   cardElement.querySelector(".card__image").srcset = `img/cards/${card.foto_2x}.png`;
+
+//   // Заголовок с HTML-разметкой
+//   cardElement.querySelector(".card__title").innerHTML = card.title;
+
+//   // Локация
+//   cardElement.querySelector(".card__location-text").textContent = card.location;
+
+//   // Продолжительность
+//   cardElement.querySelector(".card__days-text").textContent = card.days;
+//   cardElement.querySelector(".card__nights-text").textContent = card.nights;
+
+//   // Сложность (звезды)
+//   const starsIcon = cardElement.querySelector(".card__stars-icon");
+//   starsIcon.innerHTML = ""; // очищаем
+//   const starsUse = document.createElementNS("http://www.w3.org/2000/svg", "use");
+//   starsUse.setAttribute("href", `/__spritemap#sprite-stars-${card.stars}`);
+//   starsIcon.appendChild(starsUse);
+
+//   // Даты
+//   cardElement.querySelector(".card__dates-dates").textContent = card.dates;
+
+//   // Группа
+//   cardElement.querySelector(".card__group-number").textContent = card.group;
+
+//   catalog.append(cardElement);
+// };
+
+
+// export function showCards(cards) {
+//   for (const card of cards) {
+//     addCard(card);
+//   }
+// }
+
 const catalog = document.querySelector(".tours__list");
-const cardTemplate = document.querySelector("#card").content; //template
+const cardTemplate = document.querySelector("#card").content;
 
 let addCard = (card) => {
   const cardElement = cardTemplate
     .querySelector(".tours__list-item")
     .cloneNode(true);
 
-  // Изображения
-  cardElement.querySelector(".card__image-webp").srcset = `img/cards/${card.foto}.webp, img/cards/${card.foto_2x}.webp`;
-  cardElement.querySelector(".card__image-png").srcset = `img/cards/${card.foto}.png, img/cards/${card.foto_2x}.png`;
-  cardElement.querySelector(".card__image").src = `img/cards/${card.foto}.png`;
-  cardElement.querySelector(".card__image").srcset = `img/cards/${card.foto_2x}.png`;
+  // картинка
+  const sources = cardElement.querySelectorAll('source');
+  sources.forEach(source => {
+    const originalSrcset = source.getAttribute('data-srcset');
+    if (originalSrcset) {
+      const replaced = originalSrcset
+        .replaceAll('[[id]]', card.foto);
+      source.setAttribute('srcset', replaced);
+    }
+  });
 
-  // Заголовок с HTML-разметкой
+  const img = cardElement.querySelector('.card__image');
+  if (img) {
+    const rawSrc = img.dataset.src;
+    const rawSrcset = img.dataset.srcset;
+    img.src = rawSrc.replace('[[id]]', card.foto);
+    img.srcset = rawSrcset.replaceAll('[[id]]', card.foto);
+  }
+
+  // title
   cardElement.querySelector(".card__title").innerHTML = card.title;
 
-  // Локация
+  // location
   cardElement.querySelector(".card__location-text").textContent = card.location;
 
-  // Продолжительность
+  // duration
   cardElement.querySelector(".card__days-text").textContent = card.days;
   cardElement.querySelector(".card__nights-text").textContent = card.nights;
 
-  // Сложность (звезды)
+  // stars
   const starsIcon = cardElement.querySelector(".card__stars-icon");
-  starsIcon.innerHTML = ""; // очищаем
+  starsIcon.innerHTML = "";
   const starsUse = document.createElementNS("http://www.w3.org/2000/svg", "use");
   starsUse.setAttribute("href", `/__spritemap#sprite-stars-${card.stars}`);
   starsIcon.appendChild(starsUse);
 
-  // Даты
+  // dates
   cardElement.querySelector(".card__dates-dates").textContent = card.dates;
 
-  // Группа
+  // group
   cardElement.querySelector(".card__group-number").textContent = card.group;
 
   catalog.append(cardElement);
 };
-
 
 export function showCards(cards) {
   for (const card of cards) {
