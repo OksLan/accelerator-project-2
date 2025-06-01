3c2c74dfb3d60b22970ca550b4e39e268273a02d
Oksana Lanovenko <158132001+OksLan@users.noreply.github.com>
2025-06-01 20:49:22 +0400

    01/06 (2)

--- source/js/main.js
+++ source/js/main.js
@@ -116,12 +116,12 @@ const swiperReviews = new Swiper('.reviews__swiper', {
 
   breakpoints: {
     1440: {
-      slidesPerView: 1,
+      slidesPerView: 1.75,
       spaceBetween: 120,
       centeredSlides: false,
     },
     768: {
-      slidesPerView: 1,
+      slidesPerView: 1.25,
       spaceBetween: 30,
       centeredSlides: false,
     },
